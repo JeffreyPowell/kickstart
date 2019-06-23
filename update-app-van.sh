db2b45ee4ae4ca0de615fc16b38dbbb34daf234a
Jeff <Jffrypwll@googlemail.com>
2019-06-23 22:24:17 +0100

    Update update-app-van.sh

--- update-app-van.sh
+++ update-app-van.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+wget -r -O /home/pi/offgrid/cron/poll-adc.py "https://raw.githubusercontent.com/JeffreyPowell/offgrid/master/cron/poll-adc.py"
+
 python /home/pi/Desktop/send_gmail.py -t "jffrypwll@gmail.com" -s "update has run"
